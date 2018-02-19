eca57dc2fe629ec236d73e27f8e366ae5fb45da8
MSNimo <mikebenim@gmail.com>
2018-02-18 19:45:50 -0600

    Finished Learmore, starting candidate page

--- func.js
+++ func.js
@@ -1,4 +1,19 @@
-function learn_more()
-{
+function learn_more() {
      location.href = "/learnmore.html";
 } 
+
+function duckworth_go() {
+    location.href = "/duckworth.html";
+}
+
+function kirk_go() {
+    location.href = "/learnmore.html";
+}
+
+function schakowsky_go() {
+    location.href = "/learnmore.html";
+}
+
+function lasonde_go() {
+    location.href = "/learnmore.html";
+}
