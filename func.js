fc9eaecc26b76e3be756a81ce9b35043b4db373c
Lakshman Pavan Kumar Boddoju <pavanboddoju@gmail.com>
2018-02-21 22:41:11 -0600

    Added match quiz page

--- func.js
+++ func.js
@@ -1,19 +1,27 @@
 function learn_more() {
-     location.href = "/learnmore.html";
+     location.href = "learnmore.html";
 } 
 
 function duckworth_go() {
-    location.href = "/duckworth.html";
+    location.href = "duckworth.html";
 }
 
 function kirk_go() {
-    location.href = "/learnmore.html";
+    location.href = "learnmore.html";
 }
 
 function schakowsky_go() {
-    location.href = "/learnmore.html";
+    location.href = "learnmore.html";
 }
 
 function lasonde_go() {
-    location.href = "/learnmore.html";
+    location.href = "learnmore.html";
+}
+
+function match_me(){
+	location.href = "matchme.html";
+}
+
+function collect_match_me(){
+	location.href = "matchresults.html"
 }
